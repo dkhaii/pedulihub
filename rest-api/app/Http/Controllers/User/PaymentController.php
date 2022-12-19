a32435b9ed3ce1f439d46acf2da8e33b6c5d08c2
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2022-12-19 21:51:28 +0700

    backend/fitur: menambahkan payment bni gateaway di controller -belum api endpoint-

--- rest-api/app/Http/Controllers/User/PaymentController.php
+++ rest-api/app/Http/Controllers/User/PaymentController.php
@@ -4,73 +4,36 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Donation;
-use App\Models\Payment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class PaymentController extends Controller
 {
-    public function createPayment(Request $request, $id)
+    protected $midtransServerKey = 'SB-Mid-server-54nK_ysA2hg9M-43lAt8yBau';
+    protected $midtransIsProduction = false;
+    
+    protected function getDonation($inv)
     {
-        $validator = Validator::make($request->all(), [
-            'method' => 'required',
-        ]);
-
-        if($validator->fails()){
-            return response()->json([
-                'message' => 'terjadi kesalahan dalam melakukan validasi data',
-                'errors' => $validator->errors(),
-            ]);
-        }
-
-        $validated = $validator->validated();
-
-        $donation = Donation::find($id);
-        $donationId = $donation->id;
-        $donationName = $donation->name;
-        $donationNominal = $donation->nominal;
-
-        try {
-            $createPayment = Payment::create([
-                'donation_id' => $donationId,
-                'name' => $donationName,
-                'nominal' => $donationNominal,
-                'method' => $validated['method'],
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'gagal melakukan pembayaran',
-                'errors' => $e->getMessage(),
-            ]);
-        }
-
-        // if($createPayment){
-        //     $donation->status = '1';
-        // }
-
-        return response()->json([
-            'message' => 'berhasil melakukan pembayaran',
-            'payment' => $createPayment,
-        ]);
-    }
-
-    public function gopayPayment($inv)
-    {
-        \Midtrans\Config::$serverKey = 'SB-Mid-server-54nK_ysA2hg9M-43lAt8yBau';
-        \Midtrans\Config::$isProduction = false;
-
-        $user = Auth::user();
-        $userId = $user->id;
-        $name = $user->name;
-        $inv = Donation::find($inv);
+        $inv = Donation::findOrFail($inv);
         $inv_id = $inv->inv_id;
         $nominal = $inv->nominal;
+        
+        return [
+            'inv_id' => $inv_id,
+            'nominal' => $nominal                       
+        ];
+    }
+    
+    public function gopayGateaway($inv)
+    {
+        \Midtrans\Config::$serverKey = $this->midtransServerKey;
+        \Midtrans\Config::$isProduction = $this->midtransIsProduction;
+
+        $datas = $this->getDonation($inv);
 
         $params = array(
             'transaction_details' => array(
-                'order_id' => $inv_id,
-                'gross_amount' => $nominal,
+                'order_id' => $datas['inv_id'],
+                'gross_amount' => $datas['nominal'],
             ),
             'payment_type' => 'gopay',
             'gopay' => array(
@@ -79,11 +42,50 @@ class PaymentController extends Controller
             )
         );
 
-        $response = \Midtrans\CoreApi::charge($params);
+        try {
+            $response = \Midtrans\CoreApi::charge($params);
+    
+            return response()->json([
+                'message' => 'berhasil melakukan pembayaran',
+                'data' => $response,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'gagal melakukan pembayaran',
+                'errors' => $e->getMessage(),
+            ]);
+        }
+    }
 
-        return response()->json([
-            'message' => 'berhasil melakukan pembayaran',
-            'data' => $response,
-        ]);
+    public function bniGateaway($inv)
+    {
+        \Midtrans\Config::$serverKey = $this->midtransServerKey;
+        \Midtrans\Config::$isProduction = $this->midtransIsProduction;
+
+        $datas = $this->getDonation($inv);
+        
+        $params = array(
+            'payment_type' => 'bank_transfer',
+            'transaction_details' => array(
+                'order_id' => $datas['inv_id'],
+                'gross_amount' => $datas['nominal'],
+            ),
+            'bank_transfer' => array(
+                'bank' => 'bni',    
+            )
+        );
+
+        try {
+            $response = \Midtrans\CoreApi::charge($params);
+    
+            return response()->json([
+                'message' => 'berhasil melakukan pembayaran',
+                'data' => $response,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'gagal melakukan pembayaran',
+                'errors' => $e->getMessage(),
+            ]);        }
     }
 }
