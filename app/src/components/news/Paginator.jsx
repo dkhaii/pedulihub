e12413b2f6d48bc4ffb8fc7a8da17abe2bdc0bfd
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2023-01-08 11:49:54 +0700

    features: menambahkan halaman berita

---
+++ app/src/components/news/Paginator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Paginator = () => {
+  return (
+    <div className="btn-group">
+      <button className="btn">«</button>
+      <button className="btn">Page 22</button>
+      <button className="btn">»</button>
+    </div>
+  );
+};
+
+export default Paginator;
